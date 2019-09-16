648e367f4c060701f202919f37ccbcc2eaa91da2
S.Mojtaba Hosseini <S.Mojtaba1987@hotmail.com>
2019-09-16 15:27:10 +0430

    25.6.98 - add model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('کریستوفر نولان', 'بهترین کارگردان حال حاضر سینما', 'https://pmcvariety.files.wordpress.com/2018/05/nolan.jpg?w=1000'),
+    new Recipe('هیچ کاک', 'کیری ترین کارگردان',
+     'https://ichef.bbci.co.uk/news/660/cpsprodpb/4DAA/production/_104228891_mediaitem104228889.jpg')
+  ];
 
   constructor() { }
 
